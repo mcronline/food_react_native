50879bc7578e193250575b5bcbf334e0a34836d7
Michel Rozatti <contact@michelcurti.dev>
2019-12-12 07:53:25 -0300

    Added searchbar component and Yelp api access

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
 
+    const [term, setTerm ] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search',{
+            params : {
+                term : term,
+                limit : 50,
+                location : 'san jose'
+            }
+        });
+        console.log(response.data.businesses);
+        setRestaurants(response.data.businesses);
+    }
+
     return (
         <View>
-            Search View
+            <SearchBar
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+                onTermSubmit={() => searchApi()}
+            />
+            <Text>We have found {restaurants.length} restaurant(s).</Text>
         </View>
     )
 }
