fb16a1c90e350d51d11da33bb40d1ca3cf5dee14
Michel Rozatti <contact@michelcurti.dev>
2019-12-15 20:58:02 -0300

    Added ScrollView component and Navigation object

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { ScrollView, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useRestaurants from '../hooks/useRestaurants';
 import RestaurantList from '../components/RestaurantList';
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
 
-    const [term, setTerm ] = useState('');
+    const [ term, setTerm ] = useState('');
     const [restaurants, searchRestaurants] = useRestaurants('');
     
-    const restaurantFilter = price => restaurants.filter((item) => item.price);
+    const restaurantFilter = price => restaurants.filter((item) => item.price === price);
     
     return (
-        <View>
+        <>
             <SearchBar
                 term={term}
                 onTermChange={newTerm => setTerm(newTerm)}
                 onTermSubmit={() => searchRestaurants(term)}
             />
-            <RestaurantList restaurants={restaurantFilter('$')} />
-        </View>
+            <ScrollView>
+                <RestaurantList title="Low Prices" restaurants={restaurantFilter('$')} navigation={navigation} />
+                <RestaurantList title="Medium Prices" restaurants={restaurantFilter('$$')} navigation={navigation} />
+                <RestaurantList title="Higher Prices" restaurants={restaurantFilter('$$$')} navigation={navigation} />
+            </ScrollView>
+        </>
     )
 }
 
 const style = StyleSheet.create({
-
+    
 });
 
 export default SearchScreen;
