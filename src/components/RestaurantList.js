86fd70ac690b1176533325f8d7c8c0c403f0a296
Michel Rozatti <contact@michelcurti.dev>
2019-12-15 21:25:30 -0300

    Lists restaurants horizontaly based on price

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import RestaurantThumb from './RestaurantThumb';
+import { FlatList } from 'react-native-gesture-handler';
 
-const restaurantList = (restaurants) => {
-
+const restaurantList = ( { title, restaurants, navigation } ) => {
+    if(!restaurants.length)
+        return null;
+    
     return (
-        <Text></Text>
+        <View style={style.container}>
+            <Text style={style.title}>{title}</Text>
+            <FlatList
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                data={restaurants}
+                keyExtractor={(restaurant) => restaurant.id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Details', { id : item.id})}    
+                        >
+                            <RestaurantThumb key={item.id} restaurant={item} />
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        </View>
     )
 }
 
 const style = StyleSheet.create({
-
+    title :{
+        fontSize : 18,
+        fontWeight : 'bold',
+        marginLeft : 15,
+        marginBottom : 5
+    },
+    container : {
+        marginBottom : 10,
+        borderBottomWidth : 1,
+        borderBottomColor : '#CCC',
+        paddingBottom : 10
+        
+    }
 });
 
-export default restaurantList;
+export default withNavigation(restaurantList);
